faf2e10f8e9defdfe937485d38e0981615075484
Connor Cremers <connor.cremers@gmail.com>
2018-02-21 15:31:36 -0800

    Uploaded new files

--- 2018-Robot/src/Robot.cpp
+++ 2018-Robot/src/Robot.cpp
@@ -1,73 +1,169 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
+/**
+ * Example demonstrating the Position closed-loop servo.
+ * Tested with Logitech F350 USB Gamepad inserted into Driver Station]
+ *
+ * Be sure to select the correct feedback sensor using configSelectedFeedbackSensor() below.
+ *
+ * After deploying/debugging this to your RIO, first use the left Y-stick
+ * to throttle the Talon manually.  This will confirm your hardware setup.
+ * Be sure to confirm that when the Talon is driving forward (green) the
+ * position sensor is moving in a positive direction.  If this is not the cause
+ * flip the boolean input to the reverseSensor() call below.
+ *
+ * Once you've ensured your feedback device is in-phase with the motor,
+ * use the button shortcuts to servo to target position.
+ *
+ * Tweak the PID gains accordingly.
+ */
+#include "WPILib.h"
+#include "ctre/Phoenix.h"
+#include "Constants.h"
+#include "Functions.h"
+#include "AHRS.h"
 
-#include <iostream>
-#include <string>
-
-#include <IterativeRobot.h>
-#include <LiveWindow/LiveWindow.h>
-#include <SmartDashboard/SendableChooser.h>
-#include <SmartDashboard/SmartDashboard.h>
-
-class Robot : public frc::IterativeRobot {
-public:
-	void RobotInit() {
-		m_chooser.AddDefault(kAutoNameDefault, kAutoNameDefault);
-		m_chooser.AddObject(kAutoNameCustom, kAutoNameCustom);
-		frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
-	}
-
-	/*
-	 * This autonomous (along with the chooser code above) shows how to
-	 * select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * GetString line to get the auto name from the text box below the Gyro.
-	 *
-	 * You can add additional auto modes by adding additional comparisons to
-	 * the
-	 * if-else structure below with additional strings. If using the
-	 * SendableChooser make sure to add them to the chooser code above as
-	 * well.
-	 */
-	void AutonomousInit() override {
-		m_autoSelected = m_chooser.GetSelected();
-		// m_autoSelected = SmartDashboard::GetString(
-		// 		"Auto Selector", kAutoNameDefault);
-		std::cout << "Auto selected: " << m_autoSelected << std::endl;
-
-		if (m_autoSelected == kAutoNameCustom) {
-			// Custom Auto goes here
-		} else {
-			// Default Auto goes here
-		}
-	}
-
-	void AutonomousPeriodic() {
-		if (m_autoSelected == kAutoNameCustom) {
-			// Custom Auto goes here
-		} else {
-			// Default Auto goes here
-		}
-	}
-
-	void TeleopInit() {}
-
-	void TeleopPeriodic() {}
-
-	void TestPeriodic() {}
-
+using namespace nt;
+class Robot: public IterativeRobot {
 private:
-	frc::LiveWindow& m_lw = *LiveWindow::GetInstance();
-	frc::SendableChooser<std::string> m_chooser;
-	const std::string kAutoNameDefault = "Default";
-	const std::string kAutoNameCustom = "My Auto";
-	std::string m_autoSelected;
+	//drive
+	TalonSRX * lDrive1 = new TalonSRX(11);
+	TalonSRX * rDrive1 = new TalonSRX(5);
+	TalonSRX * lDrive2 = new TalonSRX(10);
+	TalonSRX * rDrive2 = new TalonSRX(6);
+	Joystick * dr = new Joystick(0);
+	float turn, throttle,lPow, rPow;
+	double kVF, kVP, kVI, kVD, kLFMod;
+
+	//auto
+	float pivot1 = 45, dis1 = 50;
+	double encToDist = 5;
+	double kDP, kDI, kDD, kDIZ, kGP, kGI, kGD, kGIZ;
+	float iE, tE, curVal, pastVal, diE, dtE, pastDist;
+	int autoStage = 0;
+	Timer autoTimer;
+	Preferences *prefs;
+	AHRS *ahrs;
+
+	void RobotInit() {
+		prefs = Preferences::GetInstance();
+
+		//drive
+		rDrive2->Set(ControlMode::Follower, 5);
+		lDrive2->Set(ControlMode::Follower, 11);
+		rDrive1->SetInverted(true);
+		rDrive2->SetInverted(true);
+		lDrive1->ConfigSelectedFeedbackSensor(FeedbackDevice::QuadEncoder,0,0);
+		rDrive1->ConfigSelectedFeedbackSensor(FeedbackDevice::QuadEncoder,0,0);
+		lDrive1->SetSensorPhase(true);
+		rDrive1->SetSensorPhase(true);
+
+        try {
+            ahrs = new AHRS(I2C::Port::kMXP);
+        } catch (std::exception& ex ) {
+            std::string err_string = "Error instantiating navX MXP:  ";
+            err_string += ex.what();
+            DriverStation::ReportError(err_string.c_str());
+        }
+
+	}
+
+
+	void AutonomousInit(){
+		ahrs->ZeroYaw();
+		autoStage = 0;
+		prefs->GetDouble("kDP", kDP);
+		prefs->GetDouble("kDI", kDI);
+		prefs->GetDouble("kDD", kDD);
+		prefs->GetDouble("kDIZ", kDIZ);
+		prefs->GetDouble("kGP", kGP);
+		prefs->GetDouble("kGI", kGI);
+		prefs->GetDouble("kGD", kGD);
+		prefs->GetDouble("kGIZ", kGIZ);
+		autoTimer.Start();
+	}
+	void AutonomousPeriodic(){
+		if(autoStage == 0){
+			curVal = ahrs->GetYaw();
+			iE = integrate(curVal, iE, pivot1, kGI, kGIZ, true);
+			tE = PIDify(pastVal, curVal, pivot1, kGP, iE, kGD, true);
+			lDrive1->Set(ControlMode::PercentOutput, tE);
+			if(abs(pivot1-curVal)>1){
+				autoTimer.Reset();
+			}
+			if(autoTimer.Get()>.5){	autoStage = 1;	}
+		}
+
+		if(autoStage == 1){
+			curVal = ahrs->GetYaw();
+			float distance = (rDrive1->GetSelectedSensorPosition(0)+lDrive1->GetSelectedSensorPosition(0))/2*encToDist;
+			diE = integrate(distance, diE, dis1, kDI, kDIZ, false);
+			float dTE = PIDify(pastDist, distance, dis1, kDP, diE, kDD, false);
+			pastDist = distance;
+			float tE = PIDify(pastVal, curVal, pivot1, kDP, 0, kDD, true);
+			pastVal = curVal;
+			if(dTE > 1){ dTE = 1; }
+			lDrive1->Set(ControlMode::PercentOutput, (dTE * (1-tE)));
+			rDrive1->Set(ControlMode::PercentOutput, (dTE * (1+tE)));
+			if(fabs(distance - dis1) < 2){
+				autoStage = 2;
+			}
+		}
+
+		if(autoStage == 2){
+			curVal = ahrs->GetYaw();
+			iE = integrate(curVal, iE, 0, kGI, kGIZ, true);
+			tE = PIDify(pastVal, curVal, 0, kGP, iE, kGD, true);
+			rDrive1->Set(ControlMode::PercentOutput, tE);
+			if(abs(0-curVal)>1){
+				autoTimer.Reset();
+			}
+			if(autoTimer.Get()>.5){	autoStage = 3;	}
+		}
+
+		if(autoStage == 3){
+			lDrive1->Set(ControlMode::PercentOutput, 0);
+			rDrive1->Set(ControlMode::PercentOutput, 0);
+		}
+	}
+	/**
+	 * This function is called periodically during operator control
+	 */
+	void TeleopInit(){
+		kVF=prefs->GetDouble("kVF", kVF);
+		kLFMod=prefs->GetDouble("kLFMod", kLFMod);
+		kVP=prefs->GetDouble("kVP", kVP);
+		kVI=prefs->GetDouble("kVI", kVI);
+		kVD=prefs->GetDouble("kVD", kVD);
+
+		lDrive1->Config_kF(0,kVF*kLFMod, kTimeoutMs);
+		lDrive1->Config_kP(0,kVP,kTimeoutMs);
+		lDrive1->Config_kI(0,kVI,kTimeoutMs);
+		lDrive1->Config_kD(0,kVD,kTimeoutMs);
+		rDrive1->Config_kF(0,kVF,kTimeoutMs);
+		rDrive1->Config_kP(0,kVP,kTimeoutMs);
+		rDrive1->Config_kI(0,kVI,kTimeoutMs);
+		rDrive1->Config_kD(0,kVD,kTimeoutMs);
+	}
+	void TeleopPeriodic() {
+		turn = adjust(dr->GetRawAxis(4)); //get the turn, scale it down
+		if(turn < 0){ turn = -turn*turn/3;	}
+		else { turn = turn*turn/3; }
+		throttle = -adjust(dr->GetRawAxis(1)); //get the throttle
+		if(throttle < 0){ throttle = -throttle*throttle;	}
+		else { throttle = throttle*throttle; }
+		lPow = throttle + turn; //simple arcade drive
+		rPow = throttle - turn;
+		//replace 1000 with whatever the max speed of the drive train is in native units
+		lPow *= 1930;
+		rPow *= 1930;
+		lDrive1->Set(ControlMode::Velocity, lPow);
+		rDrive1->Set(ControlMode::Velocity, rPow);
+		SmartDashboard::PutNumber("Motor velocity Left", lDrive1->GetSelectedSensorVelocity(0));
+		SmartDashboard::PutNumber("Motor velocity Right", rDrive1->GetSelectedSensorVelocity(0));
+		SmartDashboard::PutNumber("Desired Motor velocity Left", lPow);
+		SmartDashboard::PutNumber("Desired Motor velocity Right", rPow);
+		SmartDashboard::PutNumber("Angle", ahrs->GetYaw());
+	}
+
 };
 
 START_ROBOT_CLASS(Robot)
