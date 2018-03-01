24be5ec3041314c4042f761a02f7edb7d5977e2b
Connor Cremers <connor.cremers@gmail.com>
2018-03-01 15:00:55 -0800

    Created rough teleOp

--- 2018-Robot/src/Robot.cpp
+++ 2018-Robot/src/Robot.cpp
@@ -32,6 +32,32 @@ private:
 	Joystick * dr = new Joystick(0);
 	float turn, throttle,lPow, rPow;
 	double kVF, kVP, kVI, kVD, kLFMod;
+	bool climbing = false, PTOBool = false;
+
+	//climbing
+	DoubleSolenoid * PTO = new DoubleSolenoid(0,1);
+	DoubleSolenoid * stabilizer = new DoubleSolenoid(2,3);
+
+	Joystick *op = new Joystick(1);
+	//intake
+	TalonSRX * lIntake = new TalonSRX(0);
+	TalonSRX * rIntake = new TalonSRX(0);
+	TalonSRX * lPivot = new TalonSRX(0);
+	TalonSRX * rPivot = new TalonSRX(0);
+	AnalogInput *lPot = new AnalogInput(0);
+	AnalogInput *rPot = new AnalogInput(1);
+	double kPivot= .5, lIntakePos, rIntakePos;
+	double lIntakeWide, lIntakeNarrow, lIntakeIn, rIntakeWide, rIntakeNarrow, rIntakeIn;
+
+	//lift
+	TalonSRX * lift = new TalonSRX(0);
+	TalonSRX * arm = new TalonSRX(0);
+	DoubleSolenoid * cubeGrabber = new DoubleSolenoid(4,5);
+	double kLP, kLI, kLD, kAP, kAI, kAD;
+	int switchPos, scaleLiftPos, scaleArmPos, freeArmPos, freeLiftPos;
+	int liftPos, armPos, setLiftPos, setArmPos;
+	int objective; //0 = intake, 1 = scale, 2 = switch
+
 
 	//auto
 	float pivot1 = 45, dis1 = 50;
@@ -55,6 +81,9 @@ private:
 		rDrive1->ConfigSelectedFeedbackSensor(FeedbackDevice::QuadEncoder,0,0);
 		lDrive1->SetSensorPhase(true);
 		rDrive1->SetSensorPhase(true);
+
+		//operator
+		rIntake->Set(ControlMode::Follower, 0); //follow lIntake
 
         try {
             ahrs = new AHRS(I2C::Port::kMXP);
@@ -83,8 +112,10 @@ private:
 		if (gameData.length() > 0) {
 			if (gameData[0] == 'L') {
 				// Left code for the switch goes here
+				// Output goes to left motor
 			} else {
 				// Right code for switch goes here
+				// Output goes to right motor
 			}
 		}
 		autoTimer.Start();
@@ -151,21 +182,101 @@ private:
 		rDrive1->Config_kP(0,kVP,kTimeoutMs);
 		rDrive1->Config_kI(0,kVI,kTimeoutMs);
 		rDrive1->Config_kD(0,kVD,kTimeoutMs);
+
+		climbing = false;
 	}
 	void TeleopPeriodic() {
-		turn = adjust(dr->GetRawAxis(4)); //get the turn, scale it down
-		if(turn < 0){ turn = -turn*turn/3;	}
-		else { turn = turn*turn/3; }
-		throttle = -adjust(dr->GetRawAxis(1)); //get the throttle
-		if(throttle < 0){ throttle = -throttle*throttle;	}
-		else { throttle = throttle*throttle; }
-		lPow = throttle + turn; //simple arcade drive
-		rPow = throttle - turn;
-		//replace 1000 with whatever the max speed of the drive train is in native units
-		lPow *= 1930;
-		rPow *= 1930;
-		lDrive1->Set(ControlMode::Velocity, lPow);
-		rDrive1->Set(ControlMode::Velocity, rPow);
+		if(dr->GetRawButton(5) && dr->GetRawButton(6)){
+			climbing = true;
+			stabilizer->Set(DoubleSolenoid::kReverse);
+		}
+
+		if(climbing){
+			if(dr->GetRawButton(5)&&!dr->GetRawButton(6)){ PTO->Set(DoubleSolenoid::kForward); }
+			if(dr->GetRawButton(6)&&!dr->GetRawButton(5)){ PTO->Set(DoubleSolenoid::kForward); }
+			if(dr->GetRawButton(1)){ stabilizer->Set(DoubleSolenoid::kForward); }
+			if(dr->GetRawButton(4)){ stabilizer->Set(DoubleSolenoid::kReverse); }
+		}
+
+		if(!climbing || !PTOBool){
+			turn = adjust(dr->GetRawAxis(4)); //get the turn, scale it down
+			if(turn < 0){ turn = -turn*turn/3;	}
+			else { turn = turn*turn/3; }
+			throttle = -adjust(dr->GetRawAxis(1)); //get the throttle
+			if(throttle < 0){ throttle = -throttle*throttle;	}
+			else { throttle = throttle*throttle; }
+			lPow = throttle + turn; //simple arcade drive
+			rPow = throttle - turn;
+			//replace 1000 with whatever the max speed of the drive train is in native units
+			lPow *= 1930;
+			rPow *= 1930;
+			if(lPow != 0){	lDrive1->Set(ControlMode::Velocity, lPow); }
+			else { lDrive1->Set(ControlMode::Disabled, 0); }
+			if(rPow != 0){	rDrive1->Set(ControlMode::Velocity, rPow); }
+			else { rDrive1->Set(ControlMode::Disabled, 0); }
+		}
+		else{
+			lDrive1->Set(ControlMode::PercentOutput, throttle);
+			rDrive1->Set(ControlMode::PercentOutput, throttle);
+		}
+
+		//intake-does include driving part
+		if(objective == 2){ lIntakePos = lIntakeIn; rIntakePos = rIntakeIn; }
+		else if(op->GetRawAxis(3)>.5){ lIntakePos = lIntakeWide; rIntakePos = rIntakeWide; }
+		else{ lIntakePos = lIntakeNarrow; rIntakePos = rIntakeNarrow; }
+		if(!op->GetRawButton(5)){ lIntake->Set(ControlMode::PercentOutput, adjust(op->GetRawAxis(2))); }
+
+		//arm and scale
+		if(op->GetPOV() == 180){ objective = 2; }
+		else if(op->GetPOV() == 270){ objective = 0; }
+		else if(op->GetPOV() == 0){ objective = 1; }
+		liftPos = lift->GetSelectedSensorPosition(0);
+		armPos = arm->GetSelectedSensorPosition(0);
+		if(objective == 0){
+			if(armPos < freeArmPos){
+				setArmPos = 0; setLiftPos = 0;
+			}
+			else{
+				if(liftPos > freeLiftPos){ setArmPos = 0; setLiftPos=freeLiftPos+500; }
+				else{ setLiftPos = freeLiftPos + 500; setArmPos = freeArmPos; }
+			}
+		}
+		else if(objective == 1){
+			if(armPos > freeArmPos || liftPos > freeLiftPos){
+				setArmPos = scaleArmPos;
+				setLiftPos = scaleLiftPos;
+			}
+			else{
+				setLiftPos = scaleLiftPos;
+			}
+		}
+		else if(objective == 2){
+			if(liftPos > freeLiftPos){
+				setLiftPos = freeLiftPos+500;
+				setArmPos = switchPos;
+			}
+			else{
+				setLiftPos = freeLiftPos+500;
+				setArmPos = freeArmPos+500;
+			}
+			if(armPos > freeLiftPos){
+				setArmPos = switchPos;
+				setLiftPos = 0;
+			}
+		}
+
+		if(op->GetRawAxis(3)>.5){ cubeGrabber->Set(DoubleSolenoid::kForward); }
+		if(op->GetRawButton(6)){ cubeGrabber->Set(DoubleSolenoid::kReverse); }
+
+		if(abs(liftPos) > 1000){	lift->Set(ControlMode::Position, setLiftPos);	}
+		else{ lift->Set(ControlMode::Disabled, 0); }
+		if(abs(armPos) > 1000){ arm->Set(ControlMode::Position, setArmPos); }
+		else{ arm->Set(ControlMode::Disabled, 0); }
+
+		lIntake->Set(ControlMode::PercentOutput, kPivot*(lPot->GetVoltage()-lIntakePos));
+		rIntake->Set(ControlMode::PercentOutput, kPivot*(rPot->GetVoltage()-rIntakePos));
+
+
 		SmartDashboard::PutNumber("Motor velocity Left", lDrive1->GetSelectedSensorVelocity(0));
 		SmartDashboard::PutNumber("Motor velocity Right", rDrive1->GetSelectedSensorVelocity(0));
 		SmartDashboard::PutNumber("Desired Motor velocity Left", lPow);
